c723b11b19ac2c95522861dd5f2a59c8997f167f
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-25 16:05:44 +0200

    New git

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+// [document.querySelector('.menu'), document.querySelector('.menu1')].forEach(item => {  
+//   item.addEventListener('click', event => {
+//     document.querySelector(".header").classList.toggle("active");
+//     document.querySelector("body").classList.toggle("active");
+//   })
+// })
+
