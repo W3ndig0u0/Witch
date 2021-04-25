d7cf0b01ca18e2029753177b1976177a4e6d622e
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-25 20:51:42 +0200

    :shoe: :dog:

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,16 @@
 var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("manga");
-var modalImg = document.getElementById("img01");
+var modalImg = document.getElementById("img");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
+var close = document.getElementsByClassName("close")[0];
+
+[document.getElementById("img1"), document.getElementById("img2"), document.getElementById("img3"), document.getElementById("img4")].forEach(item => {  
+  item.addEventListener('click', event => {
   modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+  modalImg.src = item.src;
+  captionText.innerHTML = item.alt;
+})
+})
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+close.onclick = function() { 
   modal.style.display = "none";
 }
