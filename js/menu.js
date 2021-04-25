a26cfc3a6ccbae243a34ca7fbbb846b46847f169
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-25 21:37:52 +0200

    :snail:  :gun:

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,8 @@
-// [document.querySelector('.menu'), document.querySelector('.menu1')].forEach(item => {  
-//   item.addEventListener('click', event => {
-//     document.querySelector(".header").classList.toggle("active");
-//     document.querySelector("body").classList.toggle("active");
-//   })
-// })
-
+[document.getElementById("crossButton"), document.getElementById("menu-btn"), document.getElementById("mobile-menu-btn")].forEach(item => {  
+  item.addEventListener('click', event => {
+    document.querySelector(".menu").classList.toggle("active");
+    document.querySelector("main").classList.toggle("active");
+    document.querySelector("footer").classList.toggle("active");
+    console.log("YEP");
+})
+})
