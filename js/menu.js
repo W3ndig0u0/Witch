97d67594fd44be638e510f1bdce59768aa666469
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-26 20:37:22 +0200

    hmmm???

--- js/menu.js
+++ js/menu.js
@@ -9,12 +9,16 @@
 
 const nightmode = document.querySelector(".darkmode");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
 const currentTheme = localStorage.getItem("theme");
+
 if (currentTheme == "dark") {
   document.body.classList.toggle("dark-theme");
+  document.querySelector(".loader").classList.toggle("dark");
+  // document.querySelector(".menu").classList.toggle("dark");
 } else if (currentTheme == "light") {
   document.body.classList.toggle("light-theme");
+  document.querySelector(".loader").classList.toggle("light");
+  // document.querySelector(".menu").classList.toggle("light");
 }
 
 nightmode.addEventListener("click", function () {
