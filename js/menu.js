e8fd84ffca40b63d2744b680f41aee59d353e1de
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-26 19:07:11 +0200

    night mode

--- js/menu.js
+++ js/menu.js
@@ -7,12 +7,27 @@
 })
 })
 
+const nightmode = document.querySelector(".darkmode");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-document.querySelector(".nightmode").forEach(item => {  
-  item.addEventListener('click', event => {
-    console.log("NOPE");
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
 
-})
-})
-
-
+nightmode.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
