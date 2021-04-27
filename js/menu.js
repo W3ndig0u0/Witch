aea8398c9ea1f6d46430b54ff0356710cd0fe8e7
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-27 11:16:08 +0200

    Biis

--- js/menu.js
+++ js/menu.js
@@ -14,11 +14,9 @@ const currentTheme = localStorage.getItem("theme");
 if (currentTheme == "dark") {
   document.body.classList.toggle("dark-theme");
   document.querySelector(".loader").classList.toggle("dark");
-  // document.querySelector(".menu").classList.toggle("dark");
 } else if (currentTheme == "light") {
   document.body.classList.toggle("light-theme");
   document.querySelector(".loader").classList.toggle("light");
-  // document.querySelector(".menu").classList.toggle("light");
 }
 
 nightmode.addEventListener("click", function () {
