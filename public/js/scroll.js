3f255d073c1d3cae22e8cc9efe116e68ca5ee4fd
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-28 17:47:46 +0200

    clone

---
+++ public/js/scroll.js
@@ -0,0 +1,23 @@
+document.addEventListener( "DOMContentLoaded", function() {
+  
+  var div = document.getElementById( "container" );
+
+  div.addEventListener( "scroll", function() {
+    var max_scroll = this.scrollHeight - this.clientHeight;
+    var current_scroll = this.scrollTop;
+    var bottom = 80;
+
+
+    if ( current_scroll + bottom >= max_scroll ) {
+      var data = document.getElementsByClassName( "data" )[ 0 ];
+      var current = parseInt( data.dataset.current, 10 );
+      var clone = document.getElementsByClassName( "box" )[ current ];
+
+      var new_clone = clone.cloneNode( true );
+      data.appendChild( new_clone );
+
+
+      data.dataset.current = current + 1;
+    }
+  } );
+} );
