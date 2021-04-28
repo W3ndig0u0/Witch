f0d4b882c93e49f721e1dddfaadccacb497425c2
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-28 14:28:50 +0200

    Slice of Life

--- public/js/modal.js
+++ public/js/modal.js
@@ -14,3 +14,9 @@ var close = document.getElementsByClassName("close")[0];
 close.onclick = function() { 
   modal.style.display = "none";
 }
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
