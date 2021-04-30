3014541ec7fcd99a22eccb23baa6649740985e85
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-30 18:16:16 +0200

    kasd

--- public/js/menu.js
+++ public/js/menu.js
@@ -4,9 +4,19 @@
   document.getElementById("mobile-menu-btn"),
 ].forEach((item) => {
   item.addEventListener("click", (event) => {
+    document.querySelector(".menu ").style.animation = "menu-animation 1.2s forwards ease-out";
     document.querySelector(".menu").classList.toggle("active");
     document.querySelector("main").classList.toggle("active");
     document.querySelector("footer").classList.toggle("active");
+  });
+});
+
+[
+  document.getElementById("crossButton"),
+].forEach((menu) => {
+  menu.addEventListener("click", (event) => {
+    document.querySelector(".menu ").style.animation = "menu-animation 1.2s reverse both";
+    console.log("d");
   });
 });
 
