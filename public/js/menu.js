de9f42eb10c8ebfd91538a056017cc1f771021d0
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-30 13:15:29 +0200

    yes

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,13 +1,17 @@
-[document.getElementById("crossButton"), document.getElementById("menu-btn"), document.getElementById("mobile-menu-btn")].forEach(item => {  
-  item.addEventListener('click', event => {
+[
+  document.getElementById("crossButton"),
+  document.getElementById("menu-btn"),
+  document.getElementById("mobile-menu-btn"),
+].forEach((item) => {
+  item.addEventListener("click", (event) => {
     document.querySelector(".menu").classList.toggle("active");
     document.querySelector("main").classList.toggle("active");
     document.querySelector("footer").classList.toggle("active");
-})
-})
+  });
+});
 
 function changeImg(src) {
-  document.getElementById('slider').src = src;
+  document.getElementById("slider").src = src;
   console.log("img");
-  document.getElementById('slider').classList.toggle("change");
+  document.getElementById("slider").classList.toggle("change");
 }
